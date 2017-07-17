6c9d256c705d7f79a4a4b3967d919f467a113020
erinmcatee <mcateee@gmail.com>
2017-07-17 09:28:53 -0500

    add facebook pixel

--- wp-content/themes/x-child/functions.php
+++ wp-content/themes/x-child/functions.php
@@ -144,3 +144,31 @@ if ( ! function_exists( 'x_social_vertical' ) ) :
   }
 endif;
 
+
+/**
+ * Facebook pixel tracking code
+ */
+
+add_action('wp_head','p3_tracking_pixel');
+function p3_tracking_pixel() { ?>
+
+<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window,document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	 
+	 fbq('init', '322687074840706'); 
+	fbq('track', 'PageView');
+</script>
+
+<noscript>
+ <img height="1" width="1" src="https://www.facebook.com/tr?id=322687074840706&ev=PageView&noscript=1"/>
+</noscript>
+
+<?php
+}
+
