3707d70d2f45417bf67e5a43dbe5232f5a18555c
erinmcatee <mcateee@gmail.com>
2018-09-19 20:15:48 -0500

    fix MP3 SSL

--- wp-content/themes/x-child/js/p3player.js
+++ wp-content/themes/x-child/js/p3player.js
@@ -6,23 +6,23 @@
 		}, [
 			{
 				title:"From Sexual Obsession to Spiritual Awakening: 040",
-				mp3:"http://traffic.libsyn.com/p3peacepowerpurpose/P3_040.mp3"
+				mp3:"//traffic.libsyn.com/p3peacepowerpurpose/P3_040.mp3"
 			},
 			{
 				title:"Three Wise Men and Judd: 068",
-				mp3:"http://traffic.libsyn.com/p3peacepowerpurpose/powerhour_7-1-18.mp3"
+				mp3:"//traffic.libsyn.com/p3peacepowerpurpose/powerhour_7-1-18.mp3"
 			},
 			{
 				title:"Superhero vs Sinner: The Role of a Father: 043",
-				mp3:"http://traffic.libsyn.com/p3peacepowerpurpose/P3_043.mp3"
+				mp3:"//traffic.libsyn.com/p3peacepowerpurpose/P3_043.mp3"
 			},
 			{
 				title:"Pornography - Cancer of the Soul: 036",
-				mp3:"http://traffic.libsyn.com/p3peacepowerpurpose/P3_036.mp3"
+				mp3:"//traffic.libsyn.com/p3peacepowerpurpose/P3_036.mp3"
 			},
 			{
 				title:"Porn! Porn! And more porn! 049",
-				mp3:"http://traffic.libsyn.com/p3peacepowerpurpose/P3-2-18-2018.mp3"
+				mp3:"//traffic.libsyn.com/p3peacepowerpurpose/P3-2-18-2018.mp3"
 			}
 		], {
 			swfPath: "../jplayer",
